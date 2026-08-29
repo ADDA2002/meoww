8136bed16e3b07c799dafb16eddb989386ae9175
Dyad <git@dyad.sh>
2026-08-30 03:50:50 +0530

    Reverted all changes back to version 2ab50cef8dfb68dd2b6c0bcd5d316ffe7dd829c6

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -657,7 +657,7 @@ const Room = () => {
                 min={0}
                 max={duration || 100}
                 value={currentTime}
-                onInput={handleSeek}
+                onChange={handleSeek}
                 disabled={!isHost}
                 className="w-full accent-black cursor-pointer bg-gray-200 h-1.5 appearance-none border border-black"
               />
