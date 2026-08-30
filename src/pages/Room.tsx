a219607d0be8eda25bee17857046ee5e0620102c
Dyad <git@dyad.sh>
2026-08-30 18:27:08 +0530

    Reverted all changes back to version cc28d5631e2bf5d57fb28e0b1d007e15cfdf8562

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -38,8 +38,8 @@ const Room = () => {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
   const [isShuffle, setIsShuffle] = useState<boolean>(false);
 
-  // Veto (host's "Let others change what's playing" toggle) - OFF by default
-  const [vetoActive, setVetoActive] = useState<boolean>(true);
+  // Veto (host's "Let others change what's playing" toggle)
+  const [vetoActive, setVetoActive] = useState<boolean>(false);
 
   // Session state
   const [sessionEnded, setSessionEnded] = useState<boolean>(false);
