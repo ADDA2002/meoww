d0b957d42235a9a020fae0fd928051a2ab0628be
Dyad <git@dyad.sh>
2026-08-30 09:39:17 +0530

    Added missing import for Button component in Header - wrote 1 file(s)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { Radio, Menu, X } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import { Button } from "@/components/ui/button";
 
 interface HeaderProps {
   showDrawerButton?: boolean;
